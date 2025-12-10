1dac742e46ad81607f25d9d0d09e78cd83c8dc06
alwaysJOne <alwaysjone@naver.com>
2025-12-10 11:36:09 +0900

    .

--- 07_RestServer/board/src/main/java/com/kh/board/entity/Board.java
+++ 07_RestServer/board/src/main/java/com/kh/board/entity/Board.java
@@ -12,11 +12,25 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 public class Board {
-    private String boardId;
+    private Long boardId;
     private String title;
     private String contents;
     private String fileName;
     private String memberEmail;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    public void patchUpdate(String title, String contents, String fileName) {
+        if(title != null) {
+            this.title = title;
+        }
+
+        if(contents != null) {
+            this.contents = contents;
+        }
+
+        if(fileName != null) {
+            this.fileName = fileName;
+        }
+    }
 }
