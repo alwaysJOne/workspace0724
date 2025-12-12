aa2605843a03891299e60e84423bcc81bcdb0e48
alwaysJOne <alwaysjone@naver.com>
2025-12-12 11:52:23 +0900

    .

---
+++ 07_RestServer/jpa/src/main/java/com/kh/jpa/entity/Member.java
@@ -0,0 +1,42 @@
+package com.kh.jpa.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA스펙상 필수 + 외부 생성 방지
+public class Member extends BaseTimeEntity {
+
+    @Id
+    @Column(length = 30)
+    private String userId;
+
+    @Column(length = 100, nullable = false)
+    private String userPwd;
+
+    @Column(length = 15, nullable = false)
+    private String userName;
+
+    @Column(length = 255)
+    private String email;
+
+    @Column(length = 1)
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer age;
+
+    @Column(length = 13)
+    private String phone;
+
+    @Column(length = 100)
+    private String address;
+
+    public enum Gender {
+        M, F
+    }
+}
